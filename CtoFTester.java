563cb2e0c09353a321bb1d84c77980239b4fb8fd
Skai-N <snzeuton30@stuy.edu>
2021-09-23 19:26:42 -0400

    Commit by Skai-N

--- CtoFTester.java
+++ CtoFTester.java
@@ -6,11 +6,17 @@ public class CtoFTester
 {
   public static void main(String[] args)
   {
+    System.out.println(fahrenheitToCelsius(44.6));
     System.out.println(celsiusToFahrenheit(4));
   }
-  
+
   public static double celsiusToFahrenheit(double C)
   {
     return (C * 1.8) + 32;
+  }
+
+  public static double fahrenheitToCelsius(double F)
+  {
+    return (F - 32) * (5/9.0);
   }
 }
