ed2fe6eeb09b50170de0945c7bd40af3236ffe60
Skai-N <snzeuton30@stuy.edu>
2021-09-23 19:17:08 -0400

    Commit by Skai-N

---
+++ CtoFTester.java
@@ -0,0 +1,11 @@
+// parameteres: numerical values that are the temperatures in the unconverted unit (C,F)
+// parameter type: double
+// return: a double value that is the temperature in the converted unit
+
+public class CtoFTester
+{
+  public static void main(String[] args)
+  {
+
+  }
+}
